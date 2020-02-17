8b91d97240872fe547a3bd588059b969e25549c2
susanjain <60924197+susanjain@users.noreply.github.com>
2020-02-17 09:57:40 +0530

    Add files via upload

---
+++ example4.js
@@ -0,0 +1,10 @@
+function myFunction()
+{
+
+// var str=document.getElementById("demo").value;
+var str=f1.demo.value;
+
+var str1=f1.demo1.value;
+  var n= str.includes(str1);
+  document.write(n);
+}
